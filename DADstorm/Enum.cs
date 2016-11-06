463174e4a0ee934b88af539cf45add4ed59ff26d
IGORKOSIK0EFE\igorkosik <icokosik@gmail.com>
2016-11-06 14:02:27 +0000

    Add Operator, PuppetMaster and ConfigFileReader

---
+++ DADstorm/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADStorm
+{
+    enum RoutingOption
+    {
+        PRIMARY, HASHING, RANDOM
+    };
+
+    enum OperatorSpec
+    {
+        UNIQ, COUNT, DUP, FILTER, CUSTOM
+    };
+}
---
+++ DADstorm/Operator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADStorm
+{
+    class Operator
+    {
+
+        private int id;
+        private string inputSource;
+        private RoutingOption routing;
+        private int replicas;
+        private List<string> addresses;
+        private OperatorSpec operatorspec;
+
+        public Operator(int id, string inputSource, RoutingOption routing, int replicas, List<string> addresses, OperatorSpec operatorspec)
+        {
+            this.id = id;
+            this.inputSource = inputSource;
+            this.routing = routing;
+            this.replicas = replicas;
+            this.addresses = addresses;
+            this.operatorspec = operatorspec;
+        }
+
+        public void execute()
+        {
+            if (!connectionToInput())
+            {
+                connectToInput();
+            }
+            executeSomeThings();
+        }
+
+        public void connectToInput()
+        {
+
+        }
+
+        public bool connectionToInput()
+        {
+            return false;
+        }
+
+        public void executeSomeThings()
+        {
+
+        }
+
+
+    }
+}
