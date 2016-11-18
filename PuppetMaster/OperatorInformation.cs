e08be8f311cb8db493cf900962bb9fc9cffcba2b
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-11-18 12:10:54 +0000

    Create OperatorInformation class

---
+++ PuppetMaster/OperatorInformation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADstorm
+{
+    public class OperatorInformation
+    {
+        /**
+         * Basic Operator information
+         */
+        public static int id;
+        public static string name;
+        public static List<string> inputsource;
+        public static RoutingOption routing;
+        public static int replicas;
+        public static Tuple input;
+        /**
+         * FilterOperator
+         */
+        public static int fieldnumber;
+        public static string value;
+        public static FilterCondition condition;
+        /**
+         * CustomOperator
+         */
+        public static string dllLocation;
+        public static string className;
+        public static string method;
+    }
+}
