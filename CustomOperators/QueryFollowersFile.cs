1a9e014bdcd557d08fe9ac559ceed72bbd3d46d4
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-12-04 22:57:41 +0000

    Fix dll execution

--- CustomOperators/QueryFollowersFile.cs
+++ CustomOperators/QueryFollowersFile.cs
@@ -1,17 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomOperators
 {
-    class QueryFollowersFile
+    public class QueryFollowersFile
     {
+        private static Dictionary<string, List<string>> dictTweeterFollower =
+           new Dictionary<string, List<string>>();
+        private static bool dictLoaded = false;
 
-        public List<string> getFollowers()
+        private static void loadFollowersDict()
         {
-            return null;
+            string followersFilepath = @"..\..\doc\followers.data";
+
+            System.IO.StreamReader followersFile =
+            new System.IO.StreamReader(followersFilepath, true);
+            string line = followersFile.ReadLine();
+            List<string> followers;
+            while (line != null)
+            {
+                if (line[0] != '%')
+                {
+                    string[] tokens = line.Split(',');
+                    followers = new List<string>();
+                    if (dictTweeterFollower.ContainsKey(tokens[0]))
+                    {
+                        followers = dictTweeterFollower[tokens[0]];
+                    }
+                    for (int i = 1; i < tokens.Length; i++)
+                    {
+                        followers.Add(tokens[i]);
+                    }
+                    if (!dictTweeterFollower.ContainsKey(tokens[0]))
+                    {
+                        dictTweeterFollower.Add(tokens[0], followers);
+                    }
+                }
+                line = followersFile.ReadLine();
+            }
+            dictLoaded = true;
+        }
+
+        public List<List<string>> getFollowers(List<string> inputTuple)
+        {
+            List<List<string>> outputTuples = new List<List<string>>();
+            List<string> tuple;
+
+            if (!dictLoaded) loadFollowersDict();
+            if (dictTweeterFollower.ContainsKey(inputTuple[1]))
+            {
+                foreach (string follower in dictTweeterFollower[inputTuple[1]])
+                {
+                    tuple = new List<string>();
+                    tuple.Add(follower);
+                    outputTuples.Add(tuple);
+                }
+            }
+            return outputTuples;
         }
     }
 }
--- Operator/Executor/CustomExecutor.cs
+++ Operator/Executor/CustomExecutor.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CustomOperators;
 
 namespace DADstorm
 {
     class CustomExecutor : Executor
-    { 
+    {
+        private List<List<string>> outputList = new List<List<string>>();
+ 
         public CustomExecutor(OperatorInformation information)
         {
             this.information = information;
@@ -18,12 +21,30 @@ namespace DADstorm
 
         public override List<Tuple> execute()
         {
-            Assembly assembly = Assembly.LoadFile(information.dllLocation);
-            Type t = assembly.GetType(information.className);
-            dynamic method = t.GetMethod(information.method);
-            dynamic obj = Activator.CreateInstance(t);
-            List<Tuple> output = method.Invoke(obj, new object[] { });
-            return output;
+            Assembly assembly = Assembly.LoadFile(Path.GetFullPath(information.dllLocation));
+            string type = information.dllLocation.Split('.')[0] + "." + information.className;
+            Console.WriteLine(type);
+            Type t = assembly.GetType(type);
+            foreach (var m in t.GetMethods())
+                Console.WriteLine(m);
+            dynamic exec = t.GetMethod(information.method);
+            dynamic instance = Activator.CreateInstance(t);
+            foreach(Tuple tuple in input)
+            {
+                List<List<string>> output = exec.Invoke(instance, new object[] { tuple.getItems() });
+                outputList.AddRange(output);
+            }
+            return transformOutput();
+        }
+
+        private List<Tuple> transformOutput()
+        {
+            List<Tuple> result = new List<Tuple>();
+            foreach(List<string> l in outputList)
+            {
+                result.Add(new DADstorm.Tuple(l));
+            }
+            return result;
         }
 
     }
